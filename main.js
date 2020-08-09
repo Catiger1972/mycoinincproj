c45510d370071b9bb5ba3e2850fb6003666e4654
Catiger1972 <69408146+Catiger1972@users.noreply.github.com>
2020-08-08 22:05:43 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+﻿var gameData = {
+    coins: 0,
+    coinsPerClick: 1,
+    coinsPerClickCost: 10
+}
+function mineCoins() {
+    gameData.coins += gameData.coinsPerClick
+    document.getElementById("coinsMined").innerHTML = gameData.coins + " Coins Mined"
+}
+function buyCoinsPerClick() {
+    if (gameData.coins >= gameData.coinsPerClickCost) {
+        gameData.coins -= gameData.coinsPerClickCost
+        gameData.coinsPerClick += 1
+        gameData.coinsPerClickCost *= 2
+        document.getElementById("coinsMined").innerHTML = gameData.coins + " Coins Mined"
+        document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.coinsPerClick + ") Cost: " + gameData.coinsPerClickCost + " Coins"
+    }
+}
+var mainGameLoop = window.setInterval(function () {
+    mineCoins()
+}, 1000)
